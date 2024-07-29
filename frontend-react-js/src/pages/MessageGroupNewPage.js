53d9c4d0cc079eb4f0313e51a4ee36a1b8a567f1
Avinash Asok <bakaanash@yahoo.com>
2024-07-29 09:51:35 +0000

    efefefe

--- frontend-react-js/src/pages/MessageGroupNewPage.js
+++ frontend-react-js/src/pages/MessageGroupNewPage.js
@@ -8,7 +8,7 @@ import MessagesFeed from '../components/MessageFeed';
 import MessagesForm from '../components/MessageForm';
 import checkAuth from '../lib/CheckAuth';
 
-export default function MessageGroupPage() {
+export default function MessageGroupNewPage() {
   const [otherUser, setOtherUser] = React.useState([]);
   const [messageGroups, setMessageGroups] = React.useState([]);
   const [messages, setMessages] = React.useState([]);
@@ -59,7 +59,7 @@ export default function MessageGroupPage() {
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
-
+  
     loadMessageGroupsData();
     loadUserShortData();
     checkAuth(setUser);
